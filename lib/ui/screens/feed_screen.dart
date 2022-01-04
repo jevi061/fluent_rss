e01a124bc1c2c0a7a5c5385efef2dcd094f0764f
jevi061 <2843004288@qq.com>
2022-01-04 22:25:53 +0800

    reorder feeds tab

--- lib/ui/screens/feed_screen.dart
+++ lib/ui/screens/feed_screen.dart
@@ -1,34 +1,33 @@
 import 'package:fluent_rss/ui/pages/all_article_page.dart';
 import 'package:fluent_rss/ui/pages/today_article_page.dart';
 import 'package:fluent_rss/ui/pages/unread_article_page.dart';
-import 'package:fluent_rss/ui/widgets/article_tile.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
         length: 3,
+        initialIndex: 1,
         child: Scaffold(
           appBar: AppBar(
             title: const Text("Smart Feeds"),
-            bottom: TabBar(
+            bottom: const TabBar(
               tabs: [
                 Tab(
-                  text: "all",
+                  text: "today",
                 ),
                 Tab(text: "unread"),
                 Tab(
-                  text: "today",
-                )
+                  text: "all",
+                ),
               ],
             ),
           ),
-          body: TabBarView(children: [
-            AllArticlePage(),
-            UnreadArticlePage(),
+          body: const TabBarView(children: [
             TodayArticlePage(),
+            UnreadArticlePage(),
+            AllArticlePage(),
           ]),
         ));
   }
