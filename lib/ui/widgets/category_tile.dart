f80c8513182bd00aff91eb7d7f8458126f2a615d
jevi061 <2843004288@qq.com>
2022-01-07 22:14:22 +0800

    request correct channels after tap categorytile

--- lib/ui/widgets/category_tile.dart
+++ lib/ui/widgets/category_tile.dart
@@ -1,7 +1,10 @@
+import 'package:fluent_rss/business/blocs/channel/channel_bloc.dart';
+import 'package:fluent_rss/business/blocs/channel/channel_event.dart';
 import 'package:fluent_rss/data/domains/category.dart';
 import 'package:fluent_rss/router/app_router.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 
 class CategoryTile extends StatelessWidget {
@@ -31,6 +34,8 @@ class CategoryTile extends StatelessWidget {
           title: Text(category.name),
           trailing: const Icon(Icons.arrow_forward),
           onTap: () {
+            BlocProvider.of<ChannelBloc>(context)
+                .add(ChannelRequested(category.id!));
             Navigator.of(context)
                 .pushNamed(AppRouter.channelScreen, arguments: category);
           },
